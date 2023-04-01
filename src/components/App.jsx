0f2087dc36a12464a72f57703a13242db532501c
Berlinchik <davidarts11@gmail.com>
2023-04-02 00:06:30 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,28 @@ import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 
 const App = () => {
-  const [options, setFeadback] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const options = { good, neutral, bad };
+
   const optionNames = Object.keys(options);
-  const { good, neutral, bad } = options;
 
   const onLeaveFeedback = name => {
-    setFeadback(prev => {
-      return { ...prev, [name]: prev[name] + 1 };
-    });
+    switch (name) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+      default:
+        return;
+    }
   };
 
   const countTotalFeedback = () => {
